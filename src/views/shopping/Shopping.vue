15d8e67e44f004096ff617524da78f0805df2375
YuJin Kwon <tokyj515@gmail.com>
2025-02-16 13:29:30 +0900

    feat: 쇼핑몰 페이지 리펙토링

--- src/views/shopping/Shopping.vue
+++ src/views/shopping/Shopping.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
-    <h3>쇼핑몰입니다</h3>
-    <button @click="goBack" class="btn btn-gray">뒤로 가기</button>
+    <h2>쇼핑몰입니다</h2>
+    <BaseButton @click="goBack" :primary="3">뒤로 가기</BaseButton>
   </div>
 </template>
 
 <script>
-import { useRouter } from "vue-router"; // ✅ 라우터 가져오기
+import { useRouter } from "vue-router";
+import BaseButton from "@/components/base/BaseButton.vue"; // ✅ 라우터 가져오기
 
 export default {
   name: "Shopping",
+  components: {BaseButton},
   setup() {
     const router = useRouter();
 
